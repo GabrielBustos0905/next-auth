fd71e009a0433780d3eb4bbc8ee8d9c5254b2ba3
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-09-19 18:35:12 -0300

    resend email

--- lib/email.ts
+++ lib/email.ts
@@ -1,10 +1,10 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const domain = process.env.NEXT_PUBLIC_APP_URL
+// const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+    const confirmLink = `https://next-auth-lac-pi.vercel.app/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
@@ -15,7 +15,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `${domain}/auth/new-password?token=${token}`;
+    const resetLink = `https://next-auth-lac-pi.vercel.app/auth/new-password?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
